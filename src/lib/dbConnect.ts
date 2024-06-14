00430f9260b82bfb6181a21213e6b434132a1b03
Ritik Saxena <ritiksaxena124@gmail.com>
2024-06-14 13:40:13 +0530

    db connection added
    Signed-off-by: Ritik Saxena <ritiksaxena124@gmail.com>

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+  isConnected?: number;
+};
+
+const connection: ConnectionObject = {};
+
+async function dbConnect(PORT: number): Promise<void> {
+  if (connection.isConnected) {
+    console.log("Already connected to database");
+    return;
+  }
+  try {
+    const db = await mongoose.connect(process.env.DATABASE_URI || "", {});
+
+    connection.isConnected = db.connections[0].readyState;
+
+    console.log("DB connected successfully");
+  } catch (e) {
+    console.log("DB connection failed", e);
+    process.exit(1);
+  }
+}
+
+export default dbConnect;
